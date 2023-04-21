4e437d014b0484e3e8429f2ebf490237a863d006
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2023-04-21 22:39:25 +0430

    simple structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Vehicle from "./pages/Vehicle";
+import Film from "./pages/Film";
+import People from "./pages/People";
+import Species from "./pages/Species";
+import Planet from "./pages/Planet";
+import Layout from "./components/Layout";
 
 function App() {
 	return (
 		<>
 			<BrowserRouter>
+				{/* Header here */}
 				<Routes>
-					<Route path="/" element={<>Home</>} />
-					<Route path="/cars" element={<>Cars</>} />
-					<Route path="/films" element={<>Films</>} />
+					<Route element={<Layout />}>
+						<Route path="/" element={<>Home</>} />
+						<Route path="/people" element={<People />} />
+						<Route path="/films" element={<Film />} />
+						<Route path="/vehicle" element={<Vehicle />} />
+						<Route path="/species" element={<Species />} />
+						<Route path="/planet" element={<Planet />} />
+					</Route>
 				</Routes>
 			</BrowserRouter>
 		</>
